6ba81ff1ba2a580316681535742899a7a1a0d863
Tommy <52971804+ISnackable@users.noreply.github.com>
2019-12-23 16:51:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,5 +11,19 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    activity = discord.Activity(name='DEVELOPING BOT', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)
+    # channel = client.get_channel(431676381659791371)    
+    # role.mention()
+    # await channel.send(f'{channel.} test ping')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == '>>exit':
+        print('stopping bot')
+        exit()
 
 client.run(token)
