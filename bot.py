b65649f4a916e2ad4c0d7f5133642d0433d332ee
Tommy <52971804+ISnackable@users.noreply.github.com>
2020-01-04 03:05:12 +0800

    Added check owner functionality

--- bot.py
+++ bot.py
@@ -18,6 +18,13 @@ reddit = praw.Reddit(client_id=client_id,
 channels_to_update = {}
 processed_submission = []
 
+def check_owner(message):
+    if message.author.guild_permissions >= message.guild.owner.guild_permissions:
+        return True
+    else:
+        return False
+
+
 def process_link(link):
     url = re.search(r'(http|ftp|https)://([\w-]+(?:(?:.[\w-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', link)
     
@@ -47,16 +54,20 @@ async def check_history(title, link, submission_id):
     for channel in channels_to_update:
         if submission_id in processed_submission:
             # print("Already posted")
-            return True
+            break
         else:
-            inital_message = await channel.send('Retrieving latest post from r/Freegamestuff')
-            await inital_message.delete(delay=2)
-            if channels_to_update[channel] != None:
-                await channel.send('<@&'+str(channels_to_update[channel])+'> ' + str(title) + "\n" + str(link))
-            else:
-                await channel.send(str(title) + "\n" + str(link))
+            await post_freegame(channel, title, link)
     processed_submission.append(submission_id)
-    return False
+
+
+async def post_freegame(channel, title, link):
+    if channels_to_update[channel] != None:
+        await channel.send('<@&'+str(channels_to_update[channel])+'> ' + str(title) + "\n" + str(link))
+    else:
+        await channel.send(str(title) + "\n" + str(link))
+
+    if len(processed_submission) >= 20:
+        processed_submission.clear()
 
 
 @client.event
@@ -82,7 +93,7 @@ async def on_message(message):
         embed=discord.Embed(title="Commands", description="List of commands", color=0x00ffff)
         embed.add_field(name="$screenshare", value="Share screen in a discord server", inline=False)
         embed.add_field(name="$activate", value="Activate channel to be notified about free games", inline=False)
-        embed.add_field(name="$activate @role", value="Activate channel & ping the role to be notified about free games", inline=False)
+        embed.add_field(name="$activate @role", value="Activate channel & role to be notified about free games", inline=False)
         embed.add_field(name="$deactivate", value="Deactivate channel from being notified about free games", inline=False)
         embed.set_footer(text="Bot made by Ruii")
         await message.channel.send(embed=embed)
@@ -93,38 +104,41 @@ async def on_message(message):
             await message.channel.send(f"https://www.discordapp.com/channels/{message.guild.id}/{voice_channel}")
         else:
             await message.channel.send(f"Please join a voice channel first")
-    
+
     elif message.content.startswith('$activate'):
-        if not message.channel in channels_to_update:
-            if len(message.role_mentions) == 0:
-                channels_to_update[message.channel] = None
-                await message.channel.send('This channel will now be notified of new free games!')
-            elif len(message.role_mentions) == 1: 
-                role_to_update = message.role_mentions[0].id
-                channels_to_update[message.channel] = role_to_update
-                await message.channel.send(f'This channel and @{message.role_mentions[0].name} will now be notified of new free games!')
+        if check_owner(message):
+            if not message.channel in channels_to_update:
+                if len(message.role_mentions) == 0:
+                    channels_to_update[message.channel] = None
+                    await message.channel.send('This channel will now be notified of new free games!')
+                elif len(message.role_mentions) == 1: 
+                    role_to_update = message.role_mentions[0].id
+                    channels_to_update[message.channel] = role_to_update
+                    await message.channel.send(f'This channel and @{message.role_mentions[0].name} will now be notified of new free games!')
+                else:
+                    await message.channel.send("You may only add 1 role to be mentioned, refer to the commands with '$help'.")
             else:
-                await message.channel.send("You may only add 1 role to be mentioned, refer to the commands with '$help'.")
+                await message.channel.send('This channel is already in the list to receive notifications.')
         else:
-            await message.channel.send('This channel is already in the list to receive notifications.')
+            await message.channel.send(f'Sorry <@{str(message.author.id)}>, you do not have permission to use $activate.')
 
     elif message.content.startswith('$deactivate'):
-        if not message.channel in channels_to_update:
-            await message.channel.send('This channel has not been activated for free games yet!')
+        if check_owner(message):
+            if not message.channel in channels_to_update:
+                await message.channel.send('This channel has not been activated for free games yet!')
+            else:
+                del channels_to_update[message.channel]
+                await message.channel.send('This channel will now stop receiving notifications.')
         else:
-            del channels_to_update[message.channel]
-            await message.channel.send('This channel will now stop receiving notifications.')
-            
-    # if message.content.startswith('>>latest'): # command to retrieve latest free game
-    #     await message.delete()
-    #     game_title, game_link = retrieve_subreddit()
-    #     author_id = message.author.id
+            await message.channel.send(f'Sorry <@{str(message.author.id)}>, you do not have permission to use $deactivate.')
 
-    #     if await check_history(game_title, game_link):
-    #         already_posted_message = await channel.send(f"<@{str(author_id)}> The game posted above is already the latest free game.")
-    #         await already_posted_message.delete(delay=10)
-    #     else:
-    #         pass
+    elif message.content.startswith('$latest'): # Testing command
+        await message.delete()
+        if check_owner(message):
+            game_title, game_link, submission_id = retrieve_subreddit()
+            await check_history(game_title, game_link, submission_id)
+        else:
+            await message.channel.send(f'Sorry <@{str(message.author.id)}>, you do not have permission to use $deactivate.')
 
 
 async def my_background_task():
